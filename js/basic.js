da23cc319032d82c72db90a2012f9246cf09e751
linh1306 <nguyenlinh13602@gmail.com>
2023-01-02 19:35:46 +0700

    updat

--- js/basic.js
+++ js/basic.js
@@ -1,4 +1,4 @@
-var musics = [];
+var musics = {};
 
 var vn_musics = [
     'cố giang tình',
@@ -32,7 +32,11 @@ var vn_musics = [
     'chỉ anh hiểu em',
     'đừng nói yêu em',
     'kiếp duyên không thành',
-    'cứu vãn kịp không'
+    'cứu vãn kịp không',
+    'lạc trôi',
+    'ai mang cô đơn đi',
+    'hồng nhan',
+    'bạc phận',
 ];
 
 var tq_musics = [
@@ -110,51 +114,82 @@ var favorites_musics = [
     'waiting for love',
     'wake me up',
     'đào nương',
-    'ảo ảnh'
+    'ảo ảnh',
+    'lạc trôi'
 ];
+
+var mrSiro_musics = [
+    'tìm được nhau khó thế nào',
+    'bức tranh từ nước mắt',
+    'chạm đáy nỗi đau',
+    'đã từng vô giá',
+    'day dứt nỗi đau',
+    'dưới những cơn mưa',
+    'gương mặt lạ lẫm',
+    'lặng lẽ tổn thương',
+    'lắng nghe nước mắt',
+    'sống trong nỗi nhớ',
+    'tình yêu chắp vá',
+    'vô hình trong tim em'
+]
+
+vn_musics = vn_musics.concat(mrSiro_musics);
+favorites_musics = favorites_musics.concat(mrSiro_musics);
+const audioWf = document.querySelector('audio');
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const ctx = new window.AudioContext();
+const analyser = ctx.createAnalyser();
+const source = ctx.createMediaElementSource(audioWf);
+source.connect(analyser);
+source.connect(ctx.destination);
+analyser.fftSize = 64;
+const bufferLength = analyser.frequencyBinCount;
+
+let dataArray = new Uint8Array(bufferLength);
+
+
+let elements_l = [];
+let elements_r = [];
+
+
 
 var all = vn_musics.concat(tq_musics, us_musics);
 all.sort();
 var i = 0;
 all.forEach(element => {
     var name_id = "ms" + i.toString();
-    musics.push({
-        id: name_id,
-        name: element
-    });
-    i+=1
+    musics[name_id] = element;
+    i += 1
 });
 
 add_musics();
 
-function add_musics(){
-    musics.forEach(item => {
-        const main = document.getElementById("style-2");
+function add_musics() {
+    const main = document.getElementById("style-2");
+
+    for (const key in musics) {
         var class_hearth = "far fa-heart bt_list_item";
-        if(favorites_musics.includes(item.name)){
-            class_hearth = "fas fa-heart color_heart";
-        }
+        if (favorites_musics.includes(musics[key])) class_hearth = "fas fa-heart color_heart";
         if (main) {
-            var name_id = item.id;
             const item_list = document.createElement("div");
             item_list.classList.add('list__item');
-            item_list.id = name_id;
+            item_list.id = key;
             item_list.innerHTML = `
-                    <div class="list__item-name" onclick="nextTo('${item.name}','${name_id}')">
+                    <div class="list__item-name" onclick="nextTo('${musics[key]}','${key}')">
                         <i class="fal fa-list-music"></i>
-                        <p>${item.name}</p>
+                        <p>${musics[key]}</p>
                     </div>
     
-                    <i id="${name_id}-heart" class="${class_hearth}" onclick="add_remove_heart('${name_id}-heart')"></i>
-                    <i class="fas fa-times bt_list_item" onclick="remove_item('${name_id}', '${item.name}')"></i> 
+                    <i id="${key}-heart" class="${class_hearth}" onclick="add_remove_heart('${key}-heart')"></i>
+                    <i class="fas fa-times bt_list_item" onclick="remove_item('${key}', '${musics[key]}')"></i> 
             `;
             main.appendChild(item_list);
         }
-    });
+    }
 }
 
 
-function remote_list(){
+function remote_list() {
     var main = document.getElementById("container_list");
     var item = document.getElementById("style-2");
     main.removeChild(item);
@@ -168,31 +203,29 @@ function remote_list(){
 }
 
 
-function add_list_name(name_list){
+function add_list_name(name_list) {
     remote_list();
-    musics = [];
+    musics = {};
     tmp = [];
-    if(name_list=='vn_musics'){
+    if (name_list == 'vn_musics') {
         tmp = vn_musics;
-    }else if(name_list=='cn_musics'){
+    } else if (name_list == 'cn_musics') {
         tmp = tq_musics;
-    }else if(name_list=='us_musics'){
+    } else if (name_list == 'us_musics') {
         tmp = us_musics;
-    }else if(name_list == 'favorites_musics')  {
+    } else if (name_list == 'favorites_musics') {
         tmp = favorites_musics;
+    } else if(name_list  == 'mrSiro_musics'){
+        tmp = mrSiro_musics;
     }else{
         tmp = all;
     }
     var i = 0;
     tmp.forEach(element => {
         var name_id = "ms" + i.toString();
-        musics.push({
-            id: name_id,
-            name: element
-        });
-        i+=1
+        musics[name_id] = element;
+        i += 1
     });
-    musics.sort();
     add_musics();
 }
 
@@ -202,8 +235,9 @@ function updateTimeDisplay() {
     var audio = document.getElementById("myAudio");
     var currentTime = audio.currentTime;
     var duration = audio.duration;
-    var name_music = document.getElementById("name").innerHTML;
-    document.getElementById("time").innerHTML = formatTime(currentTime) + " / " + formatTime(duration);
+    if (document.getElementById("time")) {
+        document.getElementById("time").innerHTML = formatTime(currentTime) + " / " + formatTime(duration);
+    }
     if (formatTime(currentTime) == formatTime(duration)) {
         next();
     }
@@ -218,33 +252,53 @@ function formatTime(time) {
     return minutes + ":" + seconds;
 }
 
+function show_menu(){
+    var nav = document.querySelector('nav').style.display;
+    if(nav == 'block' ){
+        document.querySelector('nav').style.display = 'none';
+    }else{
+        document.querySelector('nav').style.display = 'block';
+    }
+}
+
 function playPause() {
     var audio = document.getElementById("myAudio");
     if (audio.paused) {
         audio.play();
-        document.getElementById("Pause/Play").className = "fal fa-play-circle";
+        document.getElementById("Pause_Play").className = "fal fa-pause-circle";
     } else {
         audio.pause();
-        document.getElementById("Pause/Play").className = "fal fa-pause-circle";
+        document.getElementById("Pause_Play").className = "fal fa-play-circle";
     }
 }
 
 function next() {
     var audio = document.getElementById("myAudio");
     var name_music = document.getElementById("name").innerHTML;
-    var position ;
-    var tmp = musics.find(function(item, index){
-        position = index;
-        return item.name === name_music;
-    });
-    if (position == musics.length - 1) {
-        position = 0;
-    }else{
-        position += 1;
-    }
-    var link = "music/" + musics[position].name + ".mp3";
+    var list_music = Object.values(musics);
+    var position = list_music.indexOf(name_music);
+
+    if (position == list_music.length - 1) position = 0;
+    else position += 1;
+
+    var link = "music/" + list_music[position] + ".mp3";
     audio.src = link;
-    document.getElementById("name").innerHTML = musics[position].name;
+    document.getElementById("name").innerHTML = list_music[position];
+    audio.play();
+}
+
+function back() {
+    var audio = document.getElementById("myAudio");
+    var name_music = document.getElementById("name").innerHTML;
+    var list_music = Object.values(musics);
+    var position = list_music.indexOf(name_music);
+
+    if (position == 0) position = list_music.length - 1;
+    else position -= 1;
+
+    var link = "music/" + list_music[position] + ".mp3";
+    audio.src = link;
+    document.getElementById("name").innerHTML = list_music[position];
     audio.play();
 }
 
@@ -258,12 +312,13 @@ function nextTo(link) {
 
 function add_remove_heart(name_id) {
     var name_id_father = "";
-    for (var i = 0; i < name_id.length; i++){
-        if(name_id[i]=='-')         break;
-        name_id_father+=name_id[i];
+    for (var i = 0; i < name_id.length; i++) {
+        if (name_id[i] == '-') break;
+        name_id_father += name_id[i];
     }
-    var position; 
-    var tmp = musics.find(function(item, index){
+    var position;
+    var list_music = Object.values(musics);
+    var tmp = list_music.find(function (item, index) {
         position = index;
         return item.id === name_id_father;
     });
@@ -272,26 +327,57 @@ function add_remove_heart(name_id) {
     var name_class = document.getElementById(name_id).className;
     if (name_class == "far fa-heart bt_list_item") {
         document.getElementById(name_id).className = "fas fa-heart color_heart";
-        favorites_musics.push(musics[position].name);
+        favorites_musics.push(list_music[position].name);
     }
     else {
         document.getElementById(name_id).className = "far fa-heart bt_list_item";
-        favorites_musics = favorites_musics.filter(function(element){
-            return element !== musics[position].name;
+        favorites_musics = favorites_musics.filter(function (element) {
+            return element !== list_music[position].name;
         });
     }
-    console.log(favorites_musics);
 }
 
 function remove_item(id, name_music) {
     var main = document.getElementById("style-2");
     var item = document.getElementById(id);
     main.removeChild(item);
-    var position ;
-    var tmp = musics.find(function(item, index){
+    var position;
+    var list_music = Object.values(musics);
+    var tmp = list_music.find(function (item, index) {
         position = index;
         return item.name === name_music;
     });
-    musics.splice(position, 1);
+    list_music.splice(position, 1);
+}
+
+function waveform() {
+    var name_music = document.getElementById("name").innerHTML;
+    var main = document.getElementById("container");
+    var item1 = document.getElementById('menu');
+    var item2 = document.getElementById("main_list");
+    main.removeChild(item1);
+    main.removeChild(item2);
+    main.innerHTML = `
+        <p id="name" class="name_waveform">${name_music}</p>
+        <div id="dlt_wave" class="mn__ls-item defause_waveform" onclick="delete_waveform()"><i class="fas fa-list-ul"></i></i></div>
+        <div id="id_app" class="app">
+            <div class="container_left"></div>
+            <div class="console_waveform">
+                <button class="btn_waveform" onclick = "playPause()">
+                    <i id="Pause_Play" class="fal fa-pause-circle"></i>
+                </button>
+            </div>
+            <div class="container_right"></div>
+        </div>
+    `;
+    var script = document.createElement('script');
+    script.src = 'js/waveform.js';
+    document.head.appendChild(script);
+}
+
+function remove_js(id){
+    clearInterval(id);
+    var script = document.querySelector('script[src="js/waveform.js"]');
+    script.parentNode.removeChild(script);
 }
 
---
+++ js/waveform.js
@@ -0,0 +1,114 @@
+play_wave_form();
+
+function play_wave_form(){
+    elements_l.splice(0, elements_l.length);
+    elements_r.splice(0, elements_r.length);
+    const ctn_left = document.querySelector('.container_left');
+    const ctn_right = document.querySelector('.container_right');
+    for (let i = 0; i < bufferLength; i++) {
+        const element_l = document.createElement('div');
+        element_l.classList.add('element');
+        const element_r = document.createElement('div');
+        element_r.classList.add('element');
+        elements_l.push(element_l);
+        elements_r.push(element_r);
+        ctn_left.appendChild(element_l);
+        ctn_right.appendChild(element_r);
+    }
+}
+
+
+var intervalId = setInterval(() => {
+    const btn = document.querySelector('.btn_waveform');
+    analyser.getByteFrequencyData(dataArray);
+    var value = dataArray[15];
+    var tl;
+    var vl;
+    if (dataArray[9] > 100) {
+        tl = [0, 0, 0, 0.2, 0.4, 0.6, 0.8, 1, 0.8, 0.6, 0.4, 0.2, 0, 0, 0, 0.1, 0.3, 0.5, 0.7, 0.5, 0.3, 0.1, 0, 0, 0, 0.1, 0.2, 0.3, 0.2, 0.1, 0, 0];
+        vl = [0, 0, 0, 15, 16, 15, 14, 15, 14, 15, 16, 15, 0, 0, 0, 5, 4, 6, 5, 6, 4, 5, 0, 0, 0, 17, 18, 19, 18, 17, 0, 0];
+    }
+    else {
+        tl = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0.2, 0.4, 0.6, 0.8, 0.6, 0.4, 0.2, 0, 0, 0, 0, 0.1, 0.3, 0.5, 0.7, 0.5, 0.3, 0.1, 0, 0, 0, 0, 0];
+        vl = [0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 16, 14, 15, 14, 16, 15, 0, 0, 0, 0, 5, 6, 6, 5, 6, 6, 5, 0, 0, 0, 0, 0];
+    }
+    for (let i = 0; i < bufferLength; i++) {
+        var item;
+        if (value > 0) {
+            item = Math.round(Math.random() * (11 - 5) + 5, 0);
+            if (item == 10) item = 15;
+            item = dataArray[vl[i]] * tl[i] + item
+            item = item / 255 * 100;
+        } else {
+            item = 0;
+        }
+        var vl_btn = dataArray[15] / 250 * 30;
+        elements_l[bufferLength - i - 1].style.height = item * 2 + 'px';
+        elements_r[i].style.height = item * 2 + 'px';
+    }
+    document.getElementById('name').style.fontSize = 20 + vl_btn/1.5 + 'px';
+    btn.style.width = vl_btn + 150 + 'px';
+    btn.style.height = vl_btn + 150 + 'px';
+    btn.style.fontSize = vl_btn + 150 + 'px';
+}, 0);
+
+
+function delete_waveform(){
+    var name_music = document.getElementById("name").innerHTML;
+    var main = document.getElementById("container");
+    var item1 = document.getElementById('name');
+    var item2 = document.getElementById("dlt_wave");
+    var item3 = document.getElementById("id_app");
+    main.removeChild(item1);
+    main.removeChild(item2);
+    main.removeChild(item3);
+    main.innerHTML = `
+        <div id="menu">
+            <div id="toggle" class="mn__ls-item" onclick="show_menu()">
+                <i class="far fa-bars"></i>
+            </div>
+            <nav>
+                <ul id="menu__list">
+                    <li class="mn__ls-item" onclick="add_list_name('all_musics')"><i class="fas fa-globe-americas"></i></i></i></li>
+                    <li class="mn__ls-item" onclick="add_list_name('favorites_musics')"><i class="fas fa-heart"></i></li>
+                    <li class="mn__ls-item" onclick="add_list_name('mrSiro_musics')"><i class="fas fa-cloud-rain"></i></i></li>
+                    <li class="mn__ls-item" onclick="add_list_name('vn_musics')"><i class="fab fa-vimeo-v"></i></li>
+                    <li class="mn__ls-item" onclick="add_list_name('cn_musics')"><i class="fas fa-copyright"></i></li>
+                    <li class="mn__ls-item" onclick="add_list_name('us_musics')"><i class="fas fa-union"></i></li>
+                    <li class="mn__ls-item" onclick="waveform()"><i class="fas fa-waveform-path"></i></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div id="main_list" class="list" >
+            <div class="list__hder">
+                <p>List</p>
+            </div>
+
+            <div id="container_list">
+                <div class="scrollbar" id="style-2">
+                    <!-- add item music -->
+                    <div class="force-overflow"></div>
+                </div>
+            </div>
+
+            <div class="console">
+                <p id="name">${name_music}</p>
+                <div id="time">0:00 / 0:00</div>
+                <ul class="csl__button">
+                    <li class="button">
+                        <i class="fal fa-caret-left" onclick="back()"></i>
+                    </li>
+                    <li class="button" onclick="playPause()">
+                        <i id="Pause_Play" class="fal fa-pause-circle"></i>
+                    </li>
+                    <li class="button" onclick="next()">
+                        <i class="fal fa-caret-right"></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    `
+    add_musics();
+    remove_js(intervalId);
+}
